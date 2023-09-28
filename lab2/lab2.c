15c3e7152cd1bf508c0db9185ef322ed90692f8e
David Cho <gcho13@my.bcit.ca>
2023-09-27 23:10:09 -0700

    [Modify] Lab 2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -2,19 +2,38 @@
 #include <stdlib.h>
 
 // update this with your A number
-char a_num[] = "A01351217";
+char a_num[] = "";
 
 void rotateArray(int arr[10][10], int n, int rows, int cols) {
-    
 
+    int degrees = (n/90);
+    int rotations = degrees % 4; // number of rotations
+
+    for (int i = 0; i < rotations; i++) {
+        int arr2[10][10] = {0}; // new array to store rotated values
+
+        // Perform a 90-degree rotation
+        for (int j = 0; j < rows; j++) {
+            for (int k = 0; k < cols; k++) {
+                arr2[k][rows - 1 - j] = arr[j][k];
+            }
+        }
+
+        // Copy the rotated values back to the original array
+        for (int j = 0; j < rows; j++) {
+            for (int k = 0; k < cols; k++) {
+                arr[j][k] = arr2[j][k];
+            }
+        }
+    }
     // Do NOT write any code below
     // Output the rotated array
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-	    if(arr[i][j] == 0)
-	      printf(" ");
-	    else
-	      printf("%d", arr[i][j]);
+            if(arr[i][j] == 0)
+                printf(" ");
+            else
+                printf("%d", arr[i][j]);
         }
         printf("\n");
     }
@@ -52,7 +71,7 @@ int main(int argc, char *argv[]) {
     fclose(file);
 
     printf("A%s\n", a_num);
-    
+
     // Determine the actual dimensions of the input array
     if (cols == 0) {
         cols = 10;
@@ -61,10 +80,10 @@ int main(int argc, char *argv[]) {
     printf("Input array:\n");
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-	    if(arr[i][j] == 0)
-	      printf(" ");
-	    else
-	      printf("%d", arr[i][j]);
+            if(arr[i][j] == 0)
+                printf(" ");
+            else
+                printf("%d", arr[i][j]);
         }
         printf("\n");
     }

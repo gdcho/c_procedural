a3e89d49b8b6a21ccc384679ff45a12712b6ddec
David Cho <gcho13@my.bcit.ca>
2023-11-23 16:17:08 -0800

    lab 8 updated solution

--- lab8/lab8.c
+++ lab8/lab8.c
@@ -1,42 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long long int logicaladder(long long int num1, long long int num2) {
-    long long int carry;
-    while (num2 != 0) {
-        carry = num1 & num2;
-        num1 = num1 ^ num2;
-        num2 = carry << 1;
+long long logicaladder(long long a, long long b) {
+    while (b != 0) {
+        long long carry = a & b;
+        a = a ^ b;
+        b = carry << 1;
     }
-    return num1;
+    return a;
 }
 
-long long int detectoverflow(long long int sum, int bitwidth, int shift) {
-    long long extended_sum = (long long)sum;
-    long long max = (1LL << bitwidth) - 1;
-    long long min = -(1LL << bitwidth);
+long long detectoverflow(long long a, long long b, int bitwidth, int shift) {
+    long long result = logicaladder(a, b);
 
-    if (extended_sum > max || extended_sum < min) {
-        sum = sum >> shift;
-        printf("Result after right shift: %lld\n", sum);
+    long long overflow_mask = (result ^ a) & (result ^ b) & (1LL << (bitwidth - 1));
+
+    if (overflow_mask) {
+        long long shifted_result = result >> shift;
+        printf("Result after right shift by %d: %lld\n", shift, shifted_result);
         printf("Overflow detected within the specified bitwidth.\n");
+        return shifted_result;
     } else {
-        printf("Result of addition: %lld\n", sum);
+        printf("Result of addition: %lld\n", result);
+        return result;
     }
-    return sum;
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 5) {
-        printf("Usage: <bitwidth> <num1> <num2> <shift number>\n");
-        return -1;
+        printf("Invalid number of arguments.\n");
+        return 1;
     }
 
     int bitwidth = atoi(argv[1]);
-    long long int num1 = atoll(argv[2]);
-    long long int num2 = atoll(argv[3]);
+    long long a = atoll(argv[2]);
+    long long b = atoll(argv[3]);
     int shift = atoi(argv[4]);
-    long long int sum = logicaladder(num1, num2);
-    detectoverflow(sum, bitwidth - 1, shift);
+
+    if (bitwidth != 8 && bitwidth != 16 && bitwidth != 32 && bitwidth != 64) {
+        printf("Invalid bitwidth. Supported values are 8, 16, 32, 64.\n");
+        return 1;
+    }
+
+    long long max_value = (1LL << (bitwidth - 1)) - 1;
+    long long min_value = -(1LL << (bitwidth - 1));
+
+    if (a > max_value || a < min_value || b > max_value || b < min_value) {
+        printf("Invalid value. Supported values are %lld to %lld.\n", min_value, max_value);
+        return 1;
+    }
+
+    if (shift < 0 || shift > bitwidth) {
+        printf("Invalid shift value. Supported values are 0 to %d.\n", bitwidth);
+        return 1;
+    }
+
+    detectoverflow(a, b, bitwidth, shift);
+
     return 0;
 }
